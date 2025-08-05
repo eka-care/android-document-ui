78e40fdf482688d593573183cf12c0b42884b242
UKnow-IKnow <sarojsahu014@gmail.com>
2025-08-05 18:47:29 +0530

    feat: Default to "Other" case type if none selected
    This commit modifies the `CreateCaseSheetContent` to default the case type to "Other" (represented by `CaseType.OTHER.displayName`) when creating a new case if no specific case type has been selected by the user.
    
    Additionally, the `@Preview` annotation for `CreateCaseContentPreview` has been commented out.

--- app/src/main/java/eka/care/documents/ui/components/bottomSheet/CreateCaseSheetContent.kt
+++ app/src/main/java/eka/care/documents/ui/components/bottomSheet/CreateCaseSheetContent.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.eka.ui.buttons.EkaButton
 import com.eka.ui.buttons.EkaButtonShape
@@ -66,7 +64,7 @@ fun CreateCaseSheetContent(
             onClick = {
                 selectedCaseType?.let { caseType ->
                     onCreateCase(caseName, caseType.displayName)
-                } ?: onCreateCase(caseName, "")
+                } ?: onCreateCase(caseName, CaseType.OTHER.displayName)
                 onDismiss()
             },
         )
@@ -119,10 +117,10 @@ private fun CreateCaseNameField(
     )
 }
 
-@Preview()
-@Composable
-fun CreateCaseContentPreview() {
-    MaterialTheme {
-        CreateCaseSheetContent()
-    }
-}
+//@Preview()
+//@Composable
+//fun CreateCaseContentPreview() {
+//    MaterialTheme {
+//        CreateCaseSheetContent()
+//    }
+//}
